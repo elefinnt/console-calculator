858bd5456f5b928e5cff206f77c3c2c5e9c345ff
Finn Hackshaw <hackshawf5@gmail.com>
2024-06-07 13:48:32 +1200

    Add project files.

---
+++ Calculator App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Input your first number");
+            var firstAsText = Console.ReadLine();
+            var number1 = int.Parse(firstAsText);
+
+            Console.WriteLine("Input your second number");
+            var secondAsText = Console.ReadLine();
+            var number2 = int.Parse(secondAsText);
+        }
+    }
+}
